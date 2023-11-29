1329b64fd158ea61bb04bae3fc61432c1ef20774
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-11-29 20:30:47 +0500

    Add associations of model and perform crud operations in rails console

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,15 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_create :increment_post_counters
+  after_destroy :decrement_post_counters
+
+  def increment_post_counters
+    post.increment(:comments_counter).save
+  end
+
+  def decrement_post_counters
+    post.decrement(:comments_counter).save
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  before_save :update_posts_counter
+
+  def update_posts_counter
+    self.posts_counter = posts.count
+  end
+  def recent_posts(limit: 3)
+    posts.order(created_at: :desc).limit(limit)
+  end
 end
