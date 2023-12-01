9109688d12be671dae71101e40158c1d96fa8b99
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-12-01 21:11:41 +0500

    fix validation error

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :photo, presence: true
   validates :bio, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def recent_posts(limit: 3)
     posts.order(created_at: :desc).limit(limit)
