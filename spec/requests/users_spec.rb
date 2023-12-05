9425b97d52d5f7ad5eb8cbd80880e6aa9b08d7b8
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-12-05 16:34:19 +0500

    Add test for controllers

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
+  describe 'GET /Index' do
+    it 'returns http success for Users index page' do
+      get '/users'
       expect(response).to have_http_status(:success)
+    end
+    it 'renders the index template for users' do
+      get '/users'
+      expect(response.body).to include('This page shows the users list') 
+    end
+  end
+  
+  describe 'GET /show' do
+    it 'returns http success for users show page' do
+      get '/users/:id'
+      expect(response).to have_http_status(:success)
+    end
+    it 'renders the show template for users' do
+      get '/users/:id'
+      expect(response.body).to include('Show Users with Id') 
     end
   end
 end
