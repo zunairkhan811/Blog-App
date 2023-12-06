788f6e4087c98bafbd6549635b397c38e38651a2
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-12-06 21:24:19 +0500

    Show users on /users url and styled them

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/:user_id/posts' => 'posts#index'
-  get 'users/:user_id/posts/:id' => 'posts#show'
-  get 'users' => 'users#index'
-  get 'users/:id' => 'users#show'
+  get 'users/:user_id/posts' => 'posts#index', as: 'user_posts'
+  get 'users/:user_id/posts/:id' => 'posts#show', as: 'user_post'
+  get 'users' => 'users#index', as: 'users'
+  get 'users/:id' => 'users#show', as: 'user'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
