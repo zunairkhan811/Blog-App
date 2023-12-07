1c54f5a7fa63c985981a86e7478942fa58c6bcea
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-12-07 19:29:26 +0500

    fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,20 +9,21 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def show
-    
-  end
+  def show; end
 
   private
+
   def post_params
     params.require(:post).permit(:title, :text, :comments_counter, :likes_counter, :author_id)
   end
+
   def set_user
     @user = User.find(params[:user_id])
   end
+
   def set_post
     @post = @user.posts.find(params[:id])
-  rescue ActiveRecord::RecordNotFound => error
-    redirect_to posts_path, notice: error
+  rescue ActiveRecord::RecordNotFound => e
+    redirect_to posts_path, notice: e
   end
 end
