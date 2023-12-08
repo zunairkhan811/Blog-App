d269d87c1bfb90923fdfc0fc1235e2be9075eb1b
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-12-09 00:39:30 +0500

    fix current user issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-       protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
+  helper_method :current_user
 
   def current_user
-    @current_user ||= User.find(params[:id]|| params[:user_id] ) if params[:id] || params[:user_id]
+    @current_user ||= User.first
   end
-  helper_method :current_user
 end
