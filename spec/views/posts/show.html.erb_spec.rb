588779ac4fa8f6845678a5c614272be3c1af2228
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-12-12 16:49:24 +0430

    add test for post show

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
-
-RSpec.describe 'posts/show.html.erb', type: :view do
-  # pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'Post Show Page', type: :system do
+  let(:user) do
+    User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  end
+  let(:post) do
+    user.posts.create(title: 'Post 1', text: 'Content 1')
+  end
+  before do
+    5.times do |i|
+      user.comments.create(post: post, text: "Comment #{i + 1}")
+    end
+    3.times do
+      user.likes.create(post: post)
+    end
+  end
+  describe 'Post Show Page' do
+    before { visit user_post_path(user, post) }
+    it 'shows the post title' do
+      expect(page).to have_content('Post 1')
+    end
+    it 'shows who wrote the post' do
+      expect(page).to have_content('Tom')
+    end
+    it 'shows how many comments the post has' do
+      expect(page).to have_content('Comments: 5')
+    end
+    it 'shows how many likes the post has' do
+      expect(page).to have_content('Likes: 3')
+    end
+    it 'shows the post body' do
+      expect(page).to have_content('Content 1')
+    end
+    it 'shows the username of each commentor' do
+      (1..5).each do |i|
+        expect(page).to have_content("Tom")
+      end
+    end
+    it 'shows the comment each commentor left' do
+      (1..5).each do |i|
+        expect(page).to have_content("Comment #{i}")
+      end
+    end
+  end
 end
